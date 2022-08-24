48ef6c0f861257f3c58cc467239dad299cb85fb5
rawalprakash722 <57650731+rawalprakash722@users.noreply.github.com>
2022-08-24 16:24:49 +0545

    aos animation added

--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 
+
 const Product = () => {
    
 
@@ -25,29 +26,33 @@ const Product = () => {
                 <div className="row py-4">
                    
 
-                <div className="col-lg-6 mb-4">
+                <div className="col-lg-6 mb-4" data-aos="flip-right">
                     <img src={users.image} className="rounded-circle" alt={users.title} height="400px" width="400px" />
                 </div>
-                <div className="col-lg-6">
-                    <h4 className="text-uppercase text-black-50">
+                <div className="col-lg-6" data-aos="zoom-in">
+                    <h4 className="text-uppercase text-black-50" data-aos="fade-up" data-aos-duration="1000">
                         {users.category} 
                     </h4>
-                    <h1 className="display-5">{users.title}</h1>
-                    <p className="lead fw-bolder ">
+                    <h1 className="display-5" data-aos="fade-up" data-aos-duration="2000">{users.title}</h1>
+                    <p className="lead fw-bolder " data-aos="fade-up">
                         Rating {users.rating && users.rating.rate} 
                         <i className="fa fa-star ms-3 fs-6"></i>
                         <i className="fa fa-star ms-2 fs-6"></i>
                         <i className="fa fa-star ms-2 fs-6"></i>
                         <i class="fa-regular fa-star-half-stroke ms-2 fs-6"></i>
                     </p>
-                    <h3 className="display-6 fw-bold my-4" style={{color:"#FFD700"}}>
+                    <h3 className="display-6 fw-bold my-4" data-aos="fade-up" data-aos-duration="3000" style={{color:"#FFD700"}}>
                     <i class="fa-solid fa-dollar-sign"></i> {users.price} 
                     </h3>
-                    <p className="lead">{users.description}</p>
-                    <button className="btn btn-outline-dark hovBtn px-4 py-2" >
+                    <p className="lead" data-aos="fade-up" data-aos-duration="4000">{users.description}</p>
+                    <button className="btn btn-outline-dark hovBtn px-4 py-2" data-aos="fade-right"
+     data-aos-offset="100"
+     data-aos-easing="ease-in-sine">
                         Add to Cart
                     </button>
-                    <button className="btn ms-2 px-3 py-2 " style={{backgroundColor:"#FFB4B4"}}>
+                    <button className="btn ms-2 px-3 py-2" data-aos="fade-left"
+     data-aos-offset="150"
+     data-aos-easing="ease-in-sine" style={{backgroundColor:"#FFB4B4"}}>
                         Go to Cart
                     </button>
                 </div>
